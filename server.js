d0b98bbe9e4449b9f525ce29ecf5e9e409a590e5
Gart <gmhobson@gmail.com>
2022-05-19 11:31:40 -0500

    finished lesson

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const routes = require('./controllers/');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
